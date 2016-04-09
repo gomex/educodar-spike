2814c7b4515d0f0f87fbc21e85e07d2980e92b21
Guilherme Dias <guilhermeaugusto.dias@gmail.com>
2016-04-09 06:49:22 -0300

    Remove comentários e imports usando wildcard

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,7 +4,11 @@ import generator.RubySinatraGenerator;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class Application {
     public static void main(String[] args) throws IOException {
@@ -15,16 +19,11 @@ public class Application {
 
         InputStream educodarExample = new FileInputStream(args[0]);
 
-        // Get our lexer
         EducodarLexer lexer = new EducodarLexer(new ANTLRInputStream(educodarExample));
-        // Get a list of matched tokens
         CommonTokenStream tokens = new CommonTokenStream(lexer);
-        // Pass the tokens to the parser
         EducodarParser parser = new EducodarParser(tokens);
-        // Specify our entry point
         EducodarParser.ProgramContext programContext = parser.program();
 
-        // Walks the AST and generates code
         RubySinatraGenerator rubySinatraGenerator = new RubySinatraGenerator();
         String result = (String) rubySinatraGenerator.visit(programContext);
 
