eb0371633c1c9032018a617c4a55859015de92b7
Guilherme Dias <guilhermeaugusto.dias@gmail.com>
2016-04-09 08:15:21 -0300

    Habilita geração de código usando Gradle

--- src/main/java/educodar/EducodarLexer.java
+++ src/main/java/educodar/EducodarLexer.java
@@ -1,4 +1,4 @@
-// Generated from /home/guilherme/Personal/Projects/educodar/src/main/resources/educodar/Educodar.g4 by ANTLR 4.5.1
+// Generated from Educodar.g4 by ANTLR 4.5.2
 package educodar;
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
@@ -11,7 +11,7 @@ import org.antlr.v4.runtime.misc.*;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
 public class EducodarLexer extends Lexer {
-	static { RuntimeMetaData.checkVersion("4.5.1", RuntimeMetaData.VERSION); }
+	static { RuntimeMetaData.checkVersion("4.5.2", RuntimeMetaData.VERSION); }
 
 	protected static final DFA[] _decisionToDFA;
 	protected static final PredictionContextCache _sharedContextCache =
--- src/main/java/educodar/EducodarParser.java
+++ src/main/java/educodar/EducodarParser.java
@@ -1,4 +1,4 @@
-// Generated from /home/guilherme/Personal/Projects/educodar/src/main/resources/educodar/Educodar.g4 by ANTLR 4.5.1
+// Generated from Educodar.g4 by ANTLR 4.5.2
 package educodar;
 import org.antlr.v4.runtime.atn.*;
 import org.antlr.v4.runtime.dfa.DFA;
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
 public class EducodarParser extends Parser {
-	static { RuntimeMetaData.checkVersion("4.5.1", RuntimeMetaData.VERSION); }
+	static { RuntimeMetaData.checkVersion("4.5.2", RuntimeMetaData.VERSION); }
 
 	protected static final DFA[] _decisionToDFA;
 	protected static final PredictionContextCache _sharedContextCache =
